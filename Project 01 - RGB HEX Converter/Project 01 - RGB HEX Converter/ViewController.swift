f6e6f085e5ca6804d77c09bb623c17715ba133d8
murat-so <81262445+murat-so@users.noreply.github.com>
2021-03-26 10:49:31 +0300

    last code

--- Project 01 - RGB HEX Converter/Project 01 - RGB HEX Converter/ViewController.swift
+++ Project 01 - RGB HEX Converter/Project 01 - RGB HEX Converter/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtRed: UITextField!
+    @IBOutlet weak var txtGreen: UITextField!
+    @IBOutlet weak var txtBlue: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var btnConvert: UIButton!
+    @IBOutlet weak var lblSelected: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func btnConvertClick(_ sender: UIButton) {
+        RGBtoHEX()
+    }
+    
+    func RGBtoHEX(){
+        if let redValue = UInt8(txtRed.text!){
+            if let greenValue = UInt8(txtGreen.text!){
+                if let blueValue = UInt8(txtBlue.text!){
+                    
+                    let redHex = String(format: "%2X", redValue)
+                    let greenHex = String(format: "%2X", greenValue)
+                    let blueHex = String(format: "%2X", blueValue)
+                    
+                    lblResult.text = "Hex Color: #\(redHex)\(greenHex)\(blueHex)"
+                    lblSelected.backgroundColor = UIColor(red: CGFloat(redValue)/255, green: CGFloat(greenValue)/255, blue: CGFloat(blueValue)/255, alpha: 1)
+                }
+            }
+        }
+    }
 }
 
